f2c041b75629227da45bda924665a022d3247ec2
skvuong <67217473+skvuong@users.noreply.github.com>
2020-08-02 22:28:14 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -2,45 +2,158 @@
 #server.R
 #-------------------------------------------------------------------------------
 
-library(shiny)
-library(shinyjs)
-library(wordcloud)
-library(plotly)
-library(ggplot2)
-library(stringr)
-#library(plyr)
-#library(tidyr)
-library(dplyr)
-library(datasets)
-library(ggthemes)
-library(gridExtra)
-library(RColorBrewer)
-library(gmodels)
-library(corrplot)
-library(neuralnet)
+if(!require(dplyr))
+  install.packages("dplyr")
+if(!require(ggthemes))
+  install.packages("ggthemes")
+if(!require(ggplot2))
+  install.packages("ggplot2")
+if(!require(corrplot))
+  install.packages("corrplot")
 
-# Loading data
+#Read CSV
 #-------------------------------------------------------------------------------
-top10_suburbs      <- readRDS("top10_suburbs.rds")
-correlation_matrix <- readRDS("correlation_matrix.rds")
+housedata <- read.csv("Melbourne_housing_FULL.csv",header = TRUE)
 
+
+#-------------------------------------------------------------------------------
 # Define server logic required to visualization
 #-------------------------------------------------------------------------------
 server <- function(input, output) {
-    #====Main page plot====
-    output$plot1 <- reactivePlot(function() {
-    # check for the input option
-    if (input$visualizeOption == "Top 10 Suburbs By Count") {
-        p <- ggplot(top10_suburbs, aes(reorder(Suburb,Number), Number, fill=Suburb))+
-            geom_bar(stat = "identity")+
-            theme(legend.position = "none")+
-            labs(x = "Suburb", y = "Count of Homes", title = "Top 10 Suburbs by Count of Homes")
-    }
-    else if (input$visualizeOption == "Price Correlation Matrix"){
-        p <- corrplot(correlation_matrix, method="pie", type="lower", order="hclust",
-                      col=brewer.pal(n=8, name="RdYlBu"))
-    }
-    print(p)
-    }
-)
+  output$plot1 <- reactivePlot(function() {
+
+# Plotting chart based on input option
+#-------------------------------------------------------------------------------
+
+
+#1. Top N Suburbs By Count of Homes
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "Top N Suburbs By Count of Homes") {
+#Calculate count of homes per suburb
+top_n <- housedata %>% group_by(Suburb) %>%
+  summarise(Number = n()) %>% arrange(desc(Number)) %>%
+  head(input$numRegions)
+#Plot
+p <- ggplot(top_n, aes(reorder(Suburb, Number), Number, fill = Suburb))+
+  geom_bar(stat = "identity")+
+  theme(legend.position = "none")+
+  labs(x = "Suburb", y = "Count of Homes",
+       title = "Top N Suburbs by Count of Homes")+
+  coord_flip()
 }
+
+
+#2. Top N Suburbs By Average Price
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "Top N Suburbs by Average Price of Homes") {
+#Create new dataframe - price and suburb columns
+SuburbandPrice <- housedata[c("Suburb","Price")]
+#Calculate average price per suburb
+top_n_avgprice <- SuburbandPrice %>% group_by(Suburb) %>%
+  summarise(Average = sum(Price)/n()) %>%
+  arrange(desc(Average)) %>% head(input$numRegions)
+#Plot
+p <- ggplot(top_n_avgprice, aes(reorder(Suburb, Average), Average, fill = Suburb))+
+  geom_bar(stat = "identity")+
+  theme(legend.position = "none")+
+  labs(x = "Suburb", y = "Average Price Per Home",
+       title = "Top N Suburbs by Average Price of Homes")
+}
+
+
+#3. House Price Distribution
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "House Price Distribution") {
+#Create new dataframe
+DateandPrice <- housedata[c("Suburb","Price","Date")] %>% na.omit() %>% filter(Price < 7000000)
+#Plot
+p <- ggplot(DateandPrice, aes(Price))+
+  geom_histogram(binwidth = 250000,color = "black", fill = "blue")+
+  scale_x_continuous(breaks = c(1000000,2000000,3000000,4000000,5000000,6000000,7000000),
+                     labels = c("$1m","$2m","$3m","$4m","$5m","$6m","$7m"))+
+  ggtitle("House Price Distribution")
+}
+
+
+#4. Distance from Central District Distribution
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "Distance from Central District Distribution") {
+housedata$Distance<- as.numeric(housedata$Distance)
+p <- hist(housedata$Distance, breaks=40, xlim=c(0,50), ylim=c(0,3400),
+     xlab="Distance", col="Blue", main ="Distance from Central District Distribution", las=1)
+}
+
+
+#5. No. of Bedrooms Distribution
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "No. of Bedrooms Distribution") {
+p <- hist(housedata$Bedroom2, breaks=40, xlim=c(0,10), ylim=c(0,12000),
+     xlab="No. of Bedrooms", col="Red", main="No. of Bedrooms Distribution", las=1)
+}
+
+
+#6. No. of Bathrooms Distribution
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "No. of Bathrooms Distribution") {
+p <- hist(housedata$Bathroom, breaks=40, xlim=c(0,10), ylim=c(0,4000),
+     xlab="No. of Bathrooms", col="Purple", main="No. of Bathrooms Distribution", las=1)
+}
+
+
+#7. No. of Car Lots Distribution
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "No. of Car Lots Distribution") {
+p <- hist(housedata$Car, breaks=40, xlim=c(0,10), ylim=c(0,4000),
+     xlab="No. of Carslots", col="Green", main="No. of Car Lots Distribution", las=1)
+}
+
+
+#8. Price Distribution by Home Type
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "Price Distribution by Home Type") {
+#Create New Dataframe
+housedf2 <- housedata %>%
+  select(Price, Regionname,Type,Date)
+#Plot
+p <- ggplot(housedf2, aes(Type, Price)) +
+  geom_boxplot(outlier.colour = "blue") + 
+  scale_x_discrete(labels = c('Houses','Townhouses','Units')) +
+  scale_y_continuous(breaks=seq(0,10000000,1250000)) +
+  xlab("Home Type") +
+  ylab("Price") +
+  ggtitle("Price Distribution by Home Type")
+}
+
+
+#9. Price Distribution of Regions
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "Price Distribution by Regions") {
+#Create New Dataframe
+housedf2 <- housedata %>%
+  select(Price, Regionname,Type,Date)
+#Plot
+p <- ggplot(housedf2, aes(Regionname, Price)) +
+  geom_boxplot(outlier.colour = "blue") +
+  scale_y_continuous(breaks=seq(0,10000000,1250000)) +
+  xlab("Region") +
+  ylab("Price") +
+  ggtitle("Price Distribution by Regions")
+}
+
+
+#10. Price Correlation Matrix - All Homes
+#-------------------------------------------------------------------------------
+if (input$visualizeOption == "Price Correlation Matrix") {
+housedf3 <- housedata %>%
+  select(Rooms,Bedroom2,Bathroom,Distance,Car,Landsize,BuildingArea,YearBuilt,Propertycount,Price)
+housedf3$Distance<- as.numeric(housedf3$Distance)
+housedf3$Propertycount<- as.numeric(housedf3$Propertycount)
+housedf3 <- na.omit(housedf3) 
+p <- corrplot(cor(as.matrix(housedf3)), method = "pie", type="lower")
+}
+
+#Plot the chart
+#-------------------------------------------------------------------------------
+print(p)
+}
+)}
--- ui.R
+++ ui.R
@@ -16,12 +16,22 @@ ui <- fluidPage(theme = shinytheme("flatly"),
       sidebarPanel(
         selectInput(inputId = "visualizeOption",
           label = "Housing Market - Visualization:",
-          choices = c("Top 10 Suburbs By Count",
-                      "Price Correlation Matrix"),
-          selected = "Top 10 Suburbs By Count")
+          choices = c("Top N Suburbs By Count of Homes",
+                      "Top N Suburbs by Average Price of Homes",
+                      "House Price Distribution",
+                      "Distance from Central District Distribution",
+                      "No. of Bedrooms Distribution",
+                      "No. of Bathrooms Distribution",
+                      "No. of Car Lots Distribution",
+                      "Price Distribution by Home Type",
+                      "Price Distribution by Regions",
+                      "Price Correlation Matrix"
+                      ),
+          selected =  "Top N Suburbs By Count of Homes"),
+        sliderInput("numRegions", "No. of Suburbs in bar graph:", min=1,max= 30,value=10,step=1)
         ), 
     mainPanel(
-      plotOutput(outputId = "plot1", height = "300px")
+      plotOutput(outputId = "plot1", height = "600px")
     )
   )
 ))
